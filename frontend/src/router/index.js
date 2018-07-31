f49f881e43f5fed8e3cd8b970624a4ec79b9646a
The-Jackrabbit <lsm5fm@virginia.edu>
2018-07-30 19:07:29 -0700

    Added protectedRoute component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+// Components
+import ProtectedRoute from './auth/ProtectedRoute';
 
 // Pages
 import Home from '../scenes/home/home';
@@ -11,19 +14,27 @@ import Login from '../scenes/login/login';
 import NotFoundPage from '../scenes/notFound/notFoundPage';
 import SignupConfirmation from '../scenes/signupConfirmation/signupConfirmation';
 
-const Router = () => {
-	return (
-		<Switch>
-			<Route exact path="/" component={Home}/>
-			<Route exact path="/games/" component={Games} />
-			<Route exact path="/library/" component={Library} />
-			<Route exact path="/search/" component={Search} />
-			<Route exact path="/login/" component={Login}/>
-			<Route exact path="/signup/" component={Signup}/>
-			<Route exact path="/signupConfirmation/" component={SignupConfirmation}/>
-			<Route path="/" component={NotFoundPage} />
-		</Switch>
-	);
+export default class Router extends Component {
+	constructor() {
+		super();
+		
+		this.state = {
+			auth: false,
+		};
+	}
+	
+	render() {
+		return (
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route exact path="/games/" component={Games} />
+				<ProtectedRoute exact path="/library/" component={Library} />
+				<Route exact path="/search/" component={Search}/>
+				<Route exact path="/login/" component={Login}/>
+				<Route exact path="/signup/" component={Signup}/>
+				<Route exact path="/signupConfirmation/" component={SignupConfirmation}/>
+				<Route path="/" component={NotFoundPage} />
+			</Switch>
+		);
+	}
 };
-
-export default Router;
